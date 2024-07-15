1558e346bb9b5e862e728870bcf69b7714705c3b
noodled <3495329+noodled@users.noreply.github.com>
2024-07-15 09:57:38 +0200

    Add files via upload

--- inst-zeek-extra-packagesR3.sh
+++ inst-zeek-extra-packagesR3.sh
@@ -6,13 +6,13 @@ zkg install https://github.com/anthonykasza/common-encodings --force
 ## bad zkg install https://github.com/J-Gras/add-interfaces --force
 ## bad zkg install https://github.com/J-Gras/add-json --force
 zkg install jbaggs/anomalous-dns --force
-zkg install sethhall/credit-card-exposure --force
-zkg install https://github.com/anthonykasza/bro-opendns --force
-zkg install bro/corelight/conn-burst --force
+#zkg install sethhall/credit-card-exposure --force
+#zkg install https://github.com/anthonykasza/bro-opendns --force
+#zkg install bro/corelight/conn-burst --force
 ## bro package  zkg install sethhall/credit-card-exposure --force
 zkg install cve-2021-44228 --force
 ## error 1 zkg install https://github.com/initconf/detect-kaspersky --force
-zkg install https://github.com/initconf/smtp-url-analysis --force
+#zkg install https://github.com/initconf/smtp-url-analysis --force
 ## bro package  zkg install https://github.com/initconf/phish-analysis --force
 zkg install https://github.com/corelight/detect-ransomware-filenames --force
 zkg install https://github.com/srozb/dns_axfr --force
@@ -47,7 +47,7 @@ zkg install https://github.com/sithari/icmp-exfil-detection --force
 ## error 1  zkg install https://github.com/initconf/icmp-scans --force
 #error 1 zkg install https://github.com/zeek-plugins/igmp --force
 
-zkg install https://github.com/J-Gras/intel-extensions --force
+#zkg install https://github.com/J-Gras/intel-extensions --force
 echo None of the scripts is loaded by default, i.e. zkg load intel-extensions does not enable any functionality. 
 echo To load all scripts, add the following to your local.zeek:
 #
@@ -66,7 +66,7 @@ zkg install zeek/hosom/file-extraction --force
 zkg load zeek/hosom/file-extraction
 zkg install ja3 --force
 zkg install kyd --force
-zkg install zeek/initconf/LetsEncrypt --force
+#zkg install zeek/initconf/LetsEncrypt --force
 ## error 1  zkg install https://github.com/stevesmoot/localcountry --force
 zkg install corelight/log-add-http-post-bodies --force
 zkg install corelight/log-add-vlan-everywhere --force
@@ -75,7 +75,7 @@ zkg install corelight/log-add-vlan-everywhere --force
 # you must separately load the package for it to actually do anything
 #zkg load zeek/hosom/log-filters
 
-zkg install zeek/initconf/log4j --force
+#zkg install zeek/initconf/log4j --force
 zkg install https://github.com/zeek-plugins/mdns --force
 
 echo In order to use the mDNS events and activate mDNS logging, you must explicitly load the package inside your
@@ -99,7 +99,7 @@ zkg install https://github.com/zeek/spicy-ldap --force
 zkg install https://github.com/zeek/spicy-pe --force
 zkg install https://github.com/zeek/spicy-png --force
 zkg install https://github.com/zeek/spicy-tftp -force
-zkg install https://github.com/zeek/spicy-zip --force
+#zkg install https://github.com/zeek/spicy-zip --force
 zkg install ssn-exposure --force
 zkg install suppress-ssl-notices --force
 ## bro package  zkg install https://github.com/corelight/top-dns --force
@@ -107,8 +107,8 @@ zkg install suppress-ssl-notices --force
 zkg install https://github.com/precurse/zeek-httpattacks --force
 zkg install https://github.com/captainGeech42/zeek-intel-path --force
 zkg install https://github.com/dopheide-esnet/zeek-jetdirect --force
-zkg install zeek-jpeg --force
-echo @load Zeek/JPEG
+#zkg install zeek-jpeg --force
+#echo @load Zeek/JPEG
 zkg install https://github.com/dopheide-esnet/zeek-known-hosts-with-dns --force
 ## error 1  zkg install https://github.com/dopheide-esnet/zeek-known-outbound --force
 zkg install https://github.com/reshadp/zeek-log-add-mac-addresses --force
@@ -125,50 +125,50 @@ zkg install https://github.com/stratosphereips/zeek-package-IRC --force
 zkg install https://github.com/stratosphereips/zeek-package-log-gateway-IP --force
 ## error 1  zkg install zeek-parser-CIFS-COM --force
 ## error 1  zkg install zeek-parser-CIFS-NBNS-COM --force
-zkg install zeek-parser-DHCPv4-COM --force
-zkg install zeek-parser-DHCPv6-COM --force
-zkg install zeek-parser-SSDP-COM --force
-zkg install https://github.com/ukncsc/zeek-plugin-ikev2 --force
+#zkg install zeek-parser-DHCPv4-COM --force
+#zkg install zeek-parser-DHCPv6-COM --force
+#zkg install zeek-parser-SSDP-COM --force
+##zkg install https://github.com/ukncsc/zeek-plugin-ikev2 --force
 ## error  zkg install https://github.com/0xxon/zeek-plugin-roca --force
-zkg install https://github.com/corelight/zeek-spicy-ospf --force
+#zkg install https://github.com/corelight/zeek-spicy-ospf --force
 ## some error zkg install https://github.com/corelight/zeek-spicy-openvpn --force
-zkg install https://github.com/corelight/zeek-spicy-ipsec --force
+##zkg install https://github.com/corelight/zeek-spicy-ipsec --force
 zkg install https://github.com/corelight/zeek-spicy-stun --force
 zkg install https://github.com/corelight/zeek-spicy-wireguard --force
 zkg install https://github.com/dopheide-esnet/zeek-ssh-interesting-hostnames-with-known --force
-zkg install https://github.com/corelight/zeek-xor-exe-plugin --force
+#zkg install https://github.com/corelight/zeek-xor-exe-plugin --force
 zkg install https://github.com/corelight/zerologon --force
 zkg install https://github.com/vitalyrepin/uap-bro --force
-zkg install https://github.com/AmazingPP/zeek-capwap --force
+#zkg install https://github.com/AmazingPP/zeek-capwap --force
 zkg install https://github.com/corelight/zeek-community-id --force
 zkg install https://github.com/awelzel/zeek-conn-footprint --force
 zkg install https://github.com/jsiwek/zeek-cryptomining --force
 zkg install https://github.com/jsiwek/zeek-print-log-info --force
 zkg install sethhall/unknown-mime-type-discovery --force
 zkg install jbaggs/wildcard-domain --force
-zkg install zeek/initconf/ws-discovery-dos --force
+#zkg install zeek/initconf/ws-discovery-dos --force
 zkg install https://github.com/captainGeech42/zeek-bogon --force
 zkg install https://github.com/AmazingPP/zeek-capwap --force
-zkg install https://github.com/sethhall/bro-malwarehashregistry --force
-zkg install https://github.com/AlienVault-OTX/OTX-Apps-Bro-IDS --force
-zkg install https://github.com/CriticalPathSecurity/Zeek-Intelligence-Feeds --force
+#zkg install https://github.com/sethhall/bro-malwarehashregistry --force
+#zkg install https://github.com/AlienVault-OTX/OTX-Apps-Bro-IDS --force
+#zkg install https://github.com/CriticalPathSecurity/Zeek-Intelligence-Feeds --force
 #zkg install https://github.com/sethhall/bro-securityonion/blob/master/conn-add-country.bro --force
 #zkg install https://github.com/sethhall/bro-scripts --force
 zkg install https://github.com/stevesmoot/appid --force
 zkg install https://github.com/amarokinc/bad-asn --force
-zkg install https://github.com/initconf/blacklist --force
-zkg install bro/corelight/bro-drwatson --force
-zkg install https://github.com/corelight/bro-hardware --force
-zkg install https://github.com/MITRECND/bro-http2 --force
-zkg install bro/fatemabw/bro-inventory-scripts --force
+#zkg install https://github.com/initconf/blacklist --force
+#zkg install bro/corelight/bro-drwatson --force
+#zkg install https://github.com/corelight/bro-hardware --force
+#zkg install https://github.com/MITRECND/bro-http2 --force
+#zkg install bro/fatemabw/bro-inventory-scripts --force
 zkg install https://github.com/ncsa/bro-is-darknet --force
-zkg install https://github.com/ncsa/zeek_halfduplex_analyzer --force
+#zkg install https://github.com/ncsa/zeek_halfduplex_analyzer --force
 #zkg install https://github.com/ncsa/bro-simple-scan --force
 zkg install https://github.com/corelight/detect-ransomware-filenames --force
 zkg install corelight/pingback --force
 zkg install https://github.com/theflakes/bro-large_uploads --force
 #zkg install https://github.com/hosom/bro-oui --force
-zkg install https://github.com/iamckn/oui-logging --force
+#zkg install https://github.com/iamckn/oui-logging --force
 #zkg install https://github.com/dopheide-esnet/bro-quic --force
 
 
@@ -200,6 +200,3 @@ sleep 4
 systemctl status zeek
 
 echo add2cronjob   5 * * * * bash /opt/zeek_update.sh >/dev/null 2>&1
-
-
-
--- inst-zeek-extra-packagesV2.sh
+++ inst-zeek-extra-packagesV2.sh
@@ -3,55 +3,65 @@ sudo apt-get install -y python3-pip pipx
 ##sudo pip3 install zkg
 ###sudo pipx install zkg
 echo adding zeek folder to basrc PATH sys var etc environment would be better
-echo PATH=$PATH:/root/.local/bin:/usr/local/zeek/bin >>/root/.bashrc
+echo PATH=$PATH:/root/.local/bin:/usr/local/zeek/bin >>~/.bashrc
 source ~/.bashrc
+. ~/.bashrc
+export PATH=$PATH:/root/.local/bin:/usr/local/zeek/bin
+zeek --help
 sudo /root/.local/bin/zkg autoconfig
+sudo /root/.local/pipx/venvs/zkg/bin/zkg autoconfig
 ##sudo /root/.local/bin/zkg install geoip-conn --force
 #echo setting rights to zeek folders to use pcap
 sudo setcap cap_net_raw=eip /usr/local/zeek/bin/zeek
 sudo setcap cap_net_raw=eip /usr/local/zeek/bin/capstats
 echo autoconfigure zkg
 /root/.local/bin/zkg autoconfig
+/root/.local/pipx/venvs/zkg/bin/zkg autoconfig
 echo contents of zkg config
 cat /root/.zkg/config
+cat /root/.local/pipx/venvs/zkg/.config
 #/usr/local/zeek/etc/node.cfg
 #interface=af_packet::ens33
 echo adding RFC1918 addr to networks cfg zeek
-echo 10.0.0.0/8 >>/usr/local/zeek/etc/networks.cfg         
-echo 172.16.0.0/12 >>/usr/local/zeek/etc/networks.cfg         
-echo 192.168.0.0/16 >>/usr/local/zeek/etc/networks.cfg              
+echo 10.0.0.0/8 >>/usr/local/zeek/etc/networks.cfg
+echo 172.16.0.0/12 >>/usr/local/zeek/etc/networks.cfg
+echo 192.168.0.0/16 >>/usr/local/zeek/etc/networks.cfg
 sudo systemctl stop zeekctl
 echo deploying changes with zeekctl
 /usr/local/zeek/bin/zeekctl deploy
 sudo apt-get install -y python3-pip
 sudo apt-get install -y pipx
+# /root/.local/pipx/venvs/zkg/bin/zkg
+cat /etc/os-release | grep -e'UBUNTU_CODENAME=focal' && export zkgbin=/root/.local/bin/zkg
+cat /etc/os-release | grep -e'UBUNTU_CODENAME=jammy' && export zkgbin=/root/.local/pipx/venvs/zkg/bin/zkg
 #####python3 -m pipx install zpkg
 python3 -m pipx install zkg
 echo installing zkg IRC-Zeek-package
 echo adding extra zkg packages like pex ja3 geoip-conn
-/root/.local/bin/zkg install zeek/j-gras/add-interfaces --force
-/root/.local/bin/zkg install zeek/salesforce/ja3 --force
-/root/.local/bin/zkg install geoip-conn --force
-#echo Y | /root/.local/bin/zkg install zeek/j-gras/add-interfaces
-#echo Y | /root/.local/bin/zkg install zeek/salesforce/ja3
-#echo Y | /root/.local/bin/zkg install geoip-conn
-##missing or moved /root/.local/bin/zkg install zeek/salesforce/hassh
+$zkgbin install zeek/j-gras/add-interfaces --force
+$zkgbin install zeek/salesforce/ja3 --force
+$zkgvin install geoip-conn --force
+$zkgbin install zeek/j-gras/add-interfaces --force
+$zkgbin install zeek/salesforce/ja3 --force
+$zkgbin install geoip-conn --force
+##missing or moved $zkgbin install zeek/salesforce/hassh --force
 /usr/local/zeek/bin/zeekctl deploy
 #location of installed zkg packages
-#/root/.zkg/clones/package/
+#/root/.zkg/clones/package
+#or /root/.local/pipx/venvs/zkg/clones/packages
 echo installing Malcolm zeek plugins
-wget https://raw.githubusercontent.com/cisagov/Malcolm/793abad1417ba06e3dbd4b4be628875504388e82/shared/bin/zeek_install_plugins.sh
+wget https://raw.githubusercontent.com/cisagov/Malcolm/793abad1417ba06e3dbd4b4be628875504388e82/shared/bin/zeek_install_plugins.sh -o zeek_install_plugins.sh
 bash ./zeek_install_plugins.sh
 echo fixing ja4 erronous package ja4 or missing dep
-/root/.local/bin/zkg remove ja4 --force
+$zkgbin remove ja4 --force
 echo injecting to load packages into local.zeek
 echo @load packages >> /opt/zeek/share/zeek/site/local.zeek
 echo deploying new packages and plugins
 /usr/local/zeek/bin/zeekctl deploy
 /usr/local/zeek/bin/zeekctl stop
 echo zkg checking for updated packages.
-/root/.local/bin/zkg refresh
-/root/.local/bin/zkg upgrade
+$zkgbin refresh
+$zkgbin upgrade
 echo deploying new packages and plugins
 /usr/local/zeek/bin/zeekctl deploy
 sudo systemctl stop zeek
